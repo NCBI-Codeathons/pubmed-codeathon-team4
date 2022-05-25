a1c604a4c8e770be0911fd16e89ab89959ca4511
Dan Davis <daniel.davis@nih.gov>
2022-05-25 12:44:59 -0400

    Build a scaffold for a pipeline that will produce results.
    - Can be run in a notebook like this:
    
    	from bmcodeathon.team4 import Config, Pipeline
    	config = Config.load()
    	pipeline = Pipeline(config)
    	pipeline.run()
    
    - Once you have the pipeline, you can also run parts of it.

---
+++ code/Jupyter/tests/test_config.py
@@ -0,0 +1,46 @@
+from textwrap import dedent
+
+import pytest
+
+from bmcodeathon.team4 import Config
+
+
+def test_defaults():
+    config = Config.load()
+    assert config.rate_limit == 10
+
+
+def test_with_overrides(tmp_path):
+    data = dedent("""\
+        rate_limit: 20
+    """)
+    path = tmp_path / 'team4.yaml'
+    path.write_text(data)
+
+    config = Config.load(path)
+
+    assert config.rate_limit == 20
+
+
+def test_unknown_setting_error(tmp_path):
+    data = dedent("""\
+        unknown: 20
+    """)
+    path = tmp_path / 'team4.yaml'
+    path.write_text(data)
+
+    with pytest.raises(ValueError):
+        Config.load(path)
+
+#
+# Python's typing doesn't enforce type hints - moving on
+#
+# def test_invalid_type_error(tmp_path):
+#     data = dedent("""\
+#         rate_limit: '/data/some_file.txt'
+#     """)
+#     path = tmp_path / 'team4.yaml'
+#     path.write_text(data)
+#
+#     with pytest.raises(ValueError):
+#         config = Config.load(path)
